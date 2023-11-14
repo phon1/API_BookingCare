2dcd9f1a2c5a9b0949fa2a8e94e9cf6c1d915081
Phong <thanhphong2k.bp@gmail.com>
2023-11-14 16:37:47 +0700

    test create acount

---
+++ src/services/CRUDService.js
@@ -0,0 +1,43 @@
+import bcrypt from 'bcryptjs';
+import db from '../models/index';
+
+
+const salt = bcrypt.genSaltSync(10);
+
+let createNewUser = async (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPasswordFromBcrypt = await hashUserPassword(data.password)
+            await db.User.create({
+                email: data.email,
+                password: hashPasswordFromBcrypt,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                address: data.address,
+                phonenumber: data.phonenumber,
+                gender: data.gender === '1' ? true : false,
+                roleId: data.roleId
+            })
+
+            resolve('ok! Create a new user succeed');
+
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+let hashUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            var hashPassword = await bcrypt.hashSync(password, salt);
+            resolve(hashPassword);
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+module.exports = {
+    createNewUser: createNewUser,
+}
