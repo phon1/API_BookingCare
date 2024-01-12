d6ca9561753a3c9c49ab09a3133124056c688c3b
Phong <thanhphong2k.bp@gmail.com>
2024-01-13 01:59:15 +0700

    create api get-detail-specialty-by-id

--- src/controllers/specialtyController.js
+++ src/controllers/specialtyController.js
@@ -26,7 +26,21 @@ let getAllSpecialty = async(req, res) => {
     }
 }
 
+let getdetailSpecialtyById = async(req, res) => {
+    try {
+        let info = await specialtyService.getdetailSpecialtyByIdService(req.query.id, req.query.location);
+        return res.status(200).json(info)
+    } catch(e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server!!'
+        })
+    }
+}
+
 module.exports = {
     createSpecialty: createSpecialty,
     getAllSpecialty: getAllSpecialty,
+    getdetailSpecialtyById: getdetailSpecialtyById,
 }
--- src/services/specialtyService.js
+++ src/services/specialtyService.js
@@ -51,7 +51,59 @@ let getAllSpecialtyService = (data) => {
     })
 }
 
+let getdetailSpecialtyByIdService = (inputId, location) => {
+    return new Promise (async (resolve, reject) => {
+        try {
+            if(!inputId || !location) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameter'
+                })
+            } else {
+                let data = await db.Specialty.findOne({
+                    where: {
+                        id: inputId
+                    },
+                    attributes: ['descriptuonHTML', 'descriptionMarkdown'],
+                })
+
+                if (data) {
+                    let doctorSpecialty = [];
+                    if (location === 'ALL') {
+                        doctorSpecialty = await db.Doctor_Infor.findAll({
+                            where: {
+                                specialtyId: inputId
+                            },
+                            attributes: ['doctorId', 'provinceId']
+                        })
+                    } else {
+                        // find by location
+                        doctorSpecialty = await db.Doctor_Infor.findAll({
+                            where: {
+                                specialtyId: inputId,
+                                provinceId: location
+                            },
+                            attributes: ['doctorId', 'provinceId']
+                        })
+                    }
+
+                    data.doctorSpecialty = doctorSpecialty;
+                } else data= {}
+
+                resolve({
+                    errCode: 0,
+                    errMessage: 'OK',
+                    data
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
     createSpecialtyService: createSpecialtyService,
     getAllSpecialtyService: getAllSpecialtyService,
+    getdetailSpecialtyByIdService: getdetailSpecialtyByIdService,
 }
